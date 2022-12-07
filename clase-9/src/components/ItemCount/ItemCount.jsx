829fdb2e71bb98cf8b08fee9776809b8f134a080
Francisco Pugh <franciscopugh01@gmail.com>
2022-12-06 21:14:59 -0300

    Clase 9 parte ii

--- clase-9/src/components/ItemCount/ItemCount.jsx
+++ clase-9/src/components/ItemCount/ItemCount.jsx
@@ -1,7 +1,7 @@
 import {useState} from 'react';
 
-const ItemCount = ({stock}) => {
-    const [contador, setContador] = useState(1);
+const ItemCount = ({inicial, stock, onAdd}) => {
+    const [contador, setContador] = useState(inicial);
     /*
     const boton = document.getElementById("boton1")
     console.log(boton) NO SE PUEDE*/
@@ -10,12 +10,14 @@ const ItemCount = ({stock}) => {
 
     const restar = () => contador > 1 && setContador(contador - 1)
     
+    const agregarAlCarrito = () => onAdd(contador)
+
     return (
         <div>
             <button onClick={restar} className='btn btn-light'><i className="fas fa-minus"></i></button>
                     {contador}
             <button onClick={sumar} className='btn btn-light'><i className="fas fa-plus"></i></button>
-            <button className="btn btn-dark" ><i className="fas fa-cart-plus"></i></button>
+            <button className="btn btn-dark" onClick={agregarAlCarrito}><i className="fas fa-cart-plus"></i></button>
         </div>
     );
 }
--- clase-9/src/components/ItemDetail/ItemDetail.jsx
+++ clase-9/src/components/ItemDetail/ItemDetail.jsx
@@ -1,6 +1,11 @@
 import ItemCount from "../ItemCount/ItemCount";
 import { Link } from "react-router-dom";
 const ItemDetail = ({item}) => {
+
+    const onAdd = (contador) => {
+        console.log(contador)
+    }   
+
     return (
         <div className="row g-0">
             <div className="col-md-4">
@@ -13,7 +18,7 @@ const ItemDetail = ({item}) => {
                     <p className="card-text">Marca: {item.marca} </p>
                     <p className="card-text">Precio: $ {new Intl.NumberFormat('de-DE').format(item.precio)} </p>
                     <p className="card-text">Stock: {item.stock} </p>
-                    <ItemCount stock= {item.stock}/><br/>
+                    <ItemCount inicial = {1} stock= {item.stock} onAdd={onAdd}/><br/>
                     <button className="btn btn-secondary"><Link to="/cart" className="nav-link">Finalizar compra</Link></button>
                 </div>
                 
