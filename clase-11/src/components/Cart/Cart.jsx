37f22a14ff871f0b887b8b92eea89d122c6472af
Francisco Pugh <franciscopugh01@gmail.com>
2022-12-15 21:51:40 -0300

    Clase 11 2

--- clase-11/src/components/Cart/Cart.jsx
+++ clase-11/src/components/Cart/Cart.jsx
@@ -4,7 +4,7 @@ import { useCarritoContext } from "../../context/CarritoContex";
 
 const Cart = () => {
     const {darkMode} = useDarkModeContext()
-    const {carrito} = useCarritoContext()
+    const {carrito,emptyCart, totalPrice, removeItem} = useCarritoContext()
     return (
         <>
             {carrito.length === 0 ? 
@@ -13,11 +13,35 @@ const Cart = () => {
                 <button className={`btn ${darkMode ? 'btn-secondary' : 'btn-primary'}`}><Link  className="nav-link" to={'/'}>Continuar comprando</Link></button>
             </>
             :
-            <>
-                <h1>Productos</h1>
-                <button className={`btn ${darkMode ? 'btn-secondary' : 'btn-primary'}`}><Link  className="nav-link" to={'/'}>Continuar comprando</Link></button>
-                <button className={`btn ${darkMode ? 'btn-secondary' : 'btn-primary'}`}><Link  className="nav-link" to={'/checkout'}>Finalizar Compra</Link></button>
-            </>
+            <div className="container cartContainer">
+                {
+                    carrito.map(prod => 
+                        <div className="card mb-3" key={prod.id} style={{maxWidth: '540px'}}>
+                            <div className="row g-0">
+                                <div className="col-md-4">
+                                    <img src={`../img/${prod.img}`} alt="Producto" className="img-fluid rounded-start" />
+                                </div>
+                            </div>
+                            <div className="col-md-8">
+                                <div className="cardBody">
+                                    <h5 className="card-title"> {`${prod.nombre} ${prod.marca}`}</h5>
+                                    <p className="card-text">Cantidad: {prod.cant}</p>
+                                    <p className="card-text">Precio unitario: {new Intl.NumberFormat('de-De').format(prod.precio)}</p>
+                                    <p className="card-text">Precio total: {new Intl.NumberFormat('de-De').format(prod.precio * prod.cant)}</p>
+                                </div>
+                                <button className="btn btn-danger" onClick={() => removeItem(prod.id)}>Eliminar Producto</button>
+                            </div>
+
+                        </div>      
+                )}
+
+                <div>
+                    <p>Resumen de la compra: ${ new Intl.NumberFormat('de-De').format(totalPrice())}</p>
+                    <button className="btn btn-danger" onClick={emptyCart}>Vaciar Carrito</button>
+                    <button className={`btn ${darkMode ? 'btn-secondary' : 'btn-primary'}`}><Link  className="nav-link" to={'/'}>Continuar comprando</Link></button>
+                    <button className={`btn ${darkMode ? 'btn-secondary' : 'btn-primary'}`}><Link  className="nav-link" to={'/checkout'}>Finalizar Compra</Link></button>
+                </div>   
+            </div>
 
             }
         </>
--- clase-11/src/components/CartWidget/CartWidget.jsx
+++ clase-11/src/components/CartWidget/CartWidget.jsx
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom";
+import { useCarritoContext } from "../../context/CarritoContex";
+
 const CartWidget = () => {
-  
+    const {getItemQuantity} = useCarritoContext()
+
     return (
         <>
             <button className="btn btn-secondary cartWidget">
                     <Link to={'/cart'} className="nav-link">
                         <i className="fas fa-shopping-cart fa-lg"></i>
-                        <span className="cantCarrito">0</span>
+                        {getItemQuantity() > 0 && <span className="cantCarrito">{getItemQuantity()}</span>}
                     </Link>
                         
             </button>
-                
+            
         </>
     );
 }
